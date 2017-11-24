fba2f205ba4b256ec02119e93a78b33ecb2622b3
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-11-24 23:06:40 +0100

    Download button

--- components/Crop.js
+++ components/Crop.js
@@ -21,7 +21,9 @@ export default class extends React.Component {
     })
   }
 
-  onChange = crop => this.setState({ crop })
+  onChange = crop => {
+    this.setState({ crop })
+  }
 
   rotateImage = angle => {
     const rotatedImg = rotateImg(this.props.photo, angle)
@@ -45,8 +47,6 @@ export default class extends React.Component {
         </div>
         <ReactCrop
           src={this.props.photo}
-          minWidth={20}
-          minHeight={20}
           onChange={this.onChange}
           onImageLoaded={this.onImageLoaded}
           crop={this.state.crop}
@@ -54,7 +54,29 @@ export default class extends React.Component {
         />
         <div style={{marginTop: '10px'}} className='center'>
           <Button onClick={this.newImage} value='NYTT BILDE' />
-          <Button onClick={this.saveImage} value='LAGRE BILDE' />
+          <div className={'button'}>
+            <a href={this.state.imageData} onClick={this.saveImage} download={'picture.jpg'}>LAGRE BILDE</a>
+            <style jsx>
+              {`
+              a {
+                background: #6ac4ae;
+                color: white;
+                box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14), 0 3px 1px -2px rgba(0, 0, 0, .2), 0 1px 5px 0 rgba(0, 0, 0, .12);
+                border-radius: 2px;
+                min-width: 90px;
+                padding: 0 12px;
+                font-size: 14px;
+                line-height: 22px;
+                font-weight: 500;
+                border: 0;
+                cursor: pointer;
+                height: 36px;
+                margin-right: 5px;
+                display: block;
+              }
+            `}
+            </style>
+          </div>
         </div>
       </div>
     )
