3fcff2249f99f5999a90a1448a1bd3b5594ff224
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-11-30 19:37:26 +0100

    Switches to buttons

--- components/ImageButton.js
+++ components/ImageButton.js
@@ -9,10 +9,10 @@ export default (props) => {
   }
   return (
     <div>
-      <input {...propTypes} />
+      <button {...propTypes}><img {...propTypes} /></button>
       <style jsx>
         {`
-          input {
+          button {
             box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14), 0 3px 1px -2px rgba(0, 0, 0, .2), 0 1px 5px 0 rgba(0, 0, 0, .12);
             border-radius: 2px;
             padding: 0 12px;
@@ -20,11 +20,12 @@ export default (props) => {
             cursor: pointer;
             margin-right: 5px;
           }
-          input:focus {
+          button:focus {
             outline:0;
           }
           button:active {
             outline: 0;
+            background: #f2f2f2;
           }
         `}
       </style>
