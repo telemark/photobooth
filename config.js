bb439e6cedd8442dacf61f1ec4810efb0f15848c
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-11-30 18:58:28 +0100

    updates .env

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   SSO_URL: process.env.SSO_URL || 'https://micro-auth-mock-xawzybdnnj.now.sh/login',
-  ORIGIN_URL: process.env.NOW_URL ? `${process.env.NOW_URL}/api/login` : 'http://localhost:3000/api/login',
-  SAVE_URL: process.env.NOW_URL ? `${process.env.NOW_URL}/api/save` : 'http://localhost:3000/api/save'
+  ORIGIN_URL: `${process.env.ORIGIN_URL}/api/login` || 'http://localhost:3000/api/login',
+  SAVE_URL: `${process.env.SAVE_URL}/api/save` || 'http://localhost:3000/api/save'
 }
 
 module.exports = {
